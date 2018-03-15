ec83459040d0461d2e78d67df5c9f6c8e1e8fbfb
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-15 23:17:52 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable
   module ClassMethods
-  def reset_all
-    self.all.clear
+    def reset_all
+      self.all.clear
+    end
+  
+    def count
+      self.all.count
+    end
   end
-
-  def count
-    self.all.count
-  end
-end
 end
